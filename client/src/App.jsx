0a86dc1e4337a98ad9605b12739cf6c87ae711cc
DIR\massimiliano.ranaldi <massimiliano.ranaldi@accenture.com>
2024-08-24 11:58:18 +0200

    sistemata interfaccia grafica

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,11 +14,10 @@ import paragrafoIcon6 from "./assets/importa.png";
 import paragrafoIcon5 from "./assets/esporta.png";
 import { handleImportData, handleExportData } from "./utils/dataUtils.js";
 import Sidebar from "./components/Sidebar";
-import { useLocation } from "react-router-dom"; //serve per sapere in che pagina sono
+import { useLocation } from "react-router-dom";
 
 function App() {
   const dispatch = useDispatch();
-
   const location = useLocation();
 
   // gestisce le chiamate alle api
@@ -87,10 +86,7 @@ function App() {
   };
 
   const handleSave = (newCapitolo) => {
-    //    console.log("Saving new capitolo:", newCapitolo);
-
     dispatch(fetchCapitoli());
-
     setShowAddCapitoloForm({ id: null, operazione: null });
   };
 
@@ -100,16 +96,15 @@ function App() {
       <div className="ml-60 h-screen flex flex-col">
         <div className="ml-0 p-4">
           {/* Sezione dell'indice con scorrimento */}
-          {/* Aggiunta di margine per il pannello */}
-          <div className="sticky top-0 z-10 mb-10 p-0 bg-white text-left border-yellow-500 border-b-4 border-l-4">
-            <div className=" flex items-center justify-between bg-gradient-to-r to-white from-yellow-500">
+          <div className="sticky top-0 z-10 mb-4 p-0 bg-white text-left border-yellow-500 border-b-4 border-l-4">
+            <div className="flex items-center justify-between bg-gradient-to-r from-yellow-500 to-white p-2">
               <h1 className="text-left text-3xl font-bold">
                 {`INDICE dei Capitoli ${system}`}
               </h1>
-              <div className="flex gap-0">
+              <div className="flex gap-1">
                 <button
                   title="Esporta Dati"
-                  className="bg-transparent text-white px-3 py-1 rounded-2xl hover:bg-yellow-600"
+                  className="bg-transparent text-white px-2 py-1 rounded-2xl hover:bg-yellow-600"
                   onClick={() => handleExportData(dataCapitoli, system)}
                 >
                   <img
@@ -120,7 +115,7 @@ function App() {
                 </button>
                 <button
                   title="Importa Dati"
-                  className="bg-transparent text-white px-3 py-1 rounded-2xl hover:bg-yellow-600"
+                  className="bg-transparent text-white px-2 py-1 rounded-2xl hover:bg-yellow-600"
                   onClick={() => handleImportData(dispatch)}
                 >
                   <img
@@ -131,55 +126,62 @@ function App() {
                 </button>
               </div>
             </div>
-            <div style={{ maxHeight: "250px", overflowY: "auto" }}>
-              {/* Fissa l'altezza e abilita lo scorrimento */}
+            <div
+              style={{ maxHeight: "250px", overflowY: "auto" }}
+              className="mb-4"
+            >
               <IndiceCapitoliParagrafi capitoli={dataCapitoli} />
             </div>
           </div>
 
           {/* Sezione della lista dei capitoli e paragrafi */}
-          <div className="flex-1 overflow-y-auto  bg-white border-yellow-500 border-b-4 border-l-4">
-            <div className=" mb-4 flex items-center justify-between bg-gradient-to-r to-white from-yellow-500">
+          <div className="flex-1 overflow-y-auto bg-white border-yellow-500 border-b-4 border-l-4">
+            <div className="sticky top-0 z-10 mb-4 p-2 bg-gradient-to-r from-yellow-500 to-white flex items-center justify-between">
               <h1 className="text-left text-3xl font-bold">
                 Lista dei Capitoli
               </h1>
-              <div className="ml-auto flex gap-2">
-                <div className="relative ml-auto justify-end">
-                  <button
-                    title="Aggiungi Capitolo"
-                    className="bg-transparent text-white px-3 py-1 rounded-2xl hover:bg-yellow-600"
-                    onClick={handleAddCapitoloClick}
-                  >
-                    <img
-                      src={paragrafoIcon4}
-                      alt="Aggiungi Capitolo"
-                      className="w-5 h-5"
-                    />
-                  </button>
-                  <button
-                    title="Esporta Dati"
-                    className="bg-transparent text-white px-3 py-1 rounded-2xl hover:bg-yellow-600"
-                    onClick={() => handleExportData(dataCapitoli, system)}
-                  >
-                    <img
-                      src={paragrafoIcon5}
-                      alt="Esporta Dati"
-                      className="w-5 h-5"
-                    />
-                  </button>
-                  <button
-                    title="Importa Dati"
-                    className="bg-transparent text-white px-3 py-1 rounded-2xl hover:bg-yellow-600"
-                    onClick={() => handleImportData(dispatch)}
-                  >
-                    <img
-                      src={paragrafoIcon6}
-                      alt="Importa Dati"
-                      className="w-5 h-5"
-                    />
-                  </button>
-                </div>
+              <div className="flex gap-2">
+                <button
+                  title="Aggiungi Capitolo"
+                  className="bg-transparent text-white px-2 py-1 rounded-2xl hover:bg-yellow-600"
+                  onClick={handleAddCapitoloClick}
+                >
+                  <img
+                    src={paragrafoIcon4}
+                    alt="Aggiungi Capitolo"
+                    className="w-5 h-5"
+                  />
+                </button>
+                <button
+                  title="Esporta Dati"
+                  className="bg-transparent text-white px-2 py-1 rounded-2xl hover:bg-yellow-600"
+                  onClick={() => handleExportData(dataCapitoli, system)}
+                >
+                  <img
+                    src={paragrafoIcon5}
+                    alt="Esporta Dati"
+                    className="w-5 h-5"
+                  />
+                </button>
+                <button
+                  title="Importa Dati"
+                  className="bg-transparent text-white px-2 py-1 rounded-2xl hover:bg-yellow-600"
+                  onClick={() => handleImportData(dispatch)}
+                >
+                  <img
+                    src={paragrafoIcon6}
+                    alt="Importa Dati"
+                    className="w-5 h-5"
+                  />
+                </button>
               </div>
+            </div>
+
+            <div
+              style={{ maxHeight: "calc(100vh - 250px)" }}
+              className="overflow-y-auto"
+            >
+              <ListaCapitoliParagrafi capitoli={dataCapitoli} />
             </div>
 
             {showAddCapitoloForm.operazione === "addChapter" && (
@@ -190,7 +192,6 @@ function App() {
                 onCancel={handleCancelAddCapitolo}
               />
             )}
-            <ListaCapitoliParagrafi capitoli={dataCapitoli} />
           </div>
         </div>
       </div>
